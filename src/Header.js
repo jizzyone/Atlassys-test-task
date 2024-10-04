0ad3838ffbea746711d35ede63bf6c2ded5215f3
jizzyone <165254647+jizzyone@users.noreply.github.com>
2024-10-05 01:46:23 +0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './Header.css';
-import logo from './svg/Search.svg';
 
 const Header = () => {
   return (
     <div className="header">
       <h1 id='header-subtitle'>Setting</h1>
-      <img src={Search} alt="Поиск" />;
+      <img src="../svg/Search.svg" alt="Поиск" className="profile-pic" />
       <div className="profile-section">
         <div className="icons">
           <img src="../svg/Settings.svg" alt="Настройки" className="profile-pic" />
